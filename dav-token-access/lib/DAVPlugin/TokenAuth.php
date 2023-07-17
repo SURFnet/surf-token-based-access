88654a53701fb4cef6c2ac3d4aff589e217dae26
Navid Shokri <navid.pdp11@gmail.com>
2023-07-17 14:56:25 +0330

    creating my first DAV plugin

---
+++ dav-token-access/lib/DAVPlugin/TokenAuth.php
@@ -0,0 +1,25 @@
+<?php
+namespace OCA\TokenBaseDav\DAVPlugin
+
+use Sabre\DAV;
+use Sabre\DAV\Exception\BadRequest;
+use Sabre\HTTP\RequestInterface;
+use Sabre\HTTP\ResponseInterface;
+use Sabre\VObject;
+
+class TokenAuth extends DAV\ServerPlugin
+{
+    public function initialize(DAV\Server $server) {
+		$this->server = $server;
+		$server->on('method:POST', [$this, 'httpPost'], 90);
+	}
+
+    public function httpPost(RequestInterface $request, ResponseInterface $response) {
+		$queryParams = $request->getQueryParameters();
+		if (!\array_key_exists('token-access', $queryParams)) {
+			return;
+		}
+
+		error_log("you just pocked me :)");
+	}
+}
