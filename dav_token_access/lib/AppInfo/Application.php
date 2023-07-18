b9ae1a710317f6d0e2acf4e6ee131c9b8b3ff1be
navid-shokri <123634558+navid-shokri@users.noreply.github.com>
2023-07-18 13:23:00 +0000

    restruct application.php

---
+++ dav_token_access/lib/AppInfo/Application.php
@@ -0,0 +1,36 @@
+<?php
+namespace OCA\TokenBaseDav\AppInfo;
+
+use OC\AppFramework\DependencyInjection\DIContainer;
+use OC\Core\Controller\LoginController;
+use OCA\TokenBaseDav\Services\CertificateProvider;
+use OCA\TokenBaseDav\Services\ConfigManager;
+use OCA\TokenBaseDav\Services\JWTHelper;
+use \OCP\AppFramework\App;
+use \OCA\TokenBaseDav\Controller\AuthController;
+
+class Application extends App {
+    public function __construct(array $urlParams=array()){
+        parent::__construct('tokenbasedav', $urlParams);
+
+        $container = $this->getContainer();
+		$server = $container->getServer();
+
+        $container->registerService('AuthController', function($c) {
+			$server = $c->getServer();
+			$logger = $server->getLogger();
+			$config = $server->getConfig();
+			$encodingType = $config->getSystemValue('dav.jwtEncodeType', CertificateProvider::AUTO_ENCODE_TYPE);
+			$configManager = new ConfigManager($config);
+			$certificateProvider = new CertificateProvider($configManager, $encodingType, $logger);
+			$jwtHelper = new JWTHelper($certificateProvider, $logger);
+
+            return new AuthController(
+                $c->query('AppName'),
+                $c->query('Request'),
+				$jwtHelper,
+				$logger
+            );
+        });
+    }
+}
