42b1e68d61e9cad5f6dbea1e0f815eb36eb1510d
navid-shokri <123634558+navid-shokri@users.noreply.github.com>
2023-07-25 12:28:36 +0000

    token verification tested

--- tokenbasedav/appinfo/routes.php
+++ tokenbasedav/appinfo/routes.php
@@ -1,8 +1,8 @@
 <?php
 return [
 	'routes' => [
-		['name' => "auth#issueToken", 'url' => '/auth/token', 'verb' => 'POST'],
-		['name' => "auth#test", 'url' => '/auth/test', 'verb' => 'GET'],
+		['name' => 'auth#token', 'url' => '/auth/token', 'verb' => 'POST'],
+		['name' => "auth#test", 'url' => '/auth/test', 'verb' => 'POST'],
 	],
 	'resources' => []
 ];
