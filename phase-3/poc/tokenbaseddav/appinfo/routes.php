93d4933fc867881168f1025fe4a5637a90a36cb7
Michiel de Jong <michiel@pondersource.com>
2024-05-31 15:28:50 +0200

    try to bring it back to the root route

--- phase-3/poc/tokenbaseddav/appinfo/routes.php
+++ phase-3/poc/tokenbaseddav/appinfo/routes.php
@@ -2,7 +2,7 @@
 return [
 	'routes' => [
 		['name' => 'auth#main', 'url' => '/', 'verb' => 'GET'],
-		['name' => 'auth#pick', 'url' => '/pick', 'verb' => 'POST'],
+		['name' => 'auth#pick', 'url' => '/', 'verb' => 'POST'],
 		['name' => 'auth#token', 'url' => '/auth/token', 'verb' => 'POST'],
 		['name' => "auth#test", 'url' => '/auth/test', 'verb' => 'POST'],
 	],
