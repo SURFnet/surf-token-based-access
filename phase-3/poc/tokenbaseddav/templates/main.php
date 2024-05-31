be10bf3272c8aa143230e36e21f4d1e7cbe283ce
Michiel de Jong <michiel@pondersource.com>
2024-05-31 15:09:47 +0200

    main and pick on same URL

--- phase-3/poc/tokenbaseddav/templates/main.php
+++ phase-3/poc/tokenbaseddav/templates/main.php
@@ -13,7 +13,7 @@ Util::addStyle($appId, 'style');
 <div id="app">
 	<div id="app-content">
 		please pick a folder to share (coming soon)
-		<form method="POST" action="<?php echo $_['urlGenerator']->linkToRoute('tokenbaseddav.AuthController.pick'); ?>">
+		<form method="POST" action="#">
 			<input type="submit"/>
 		</form>
 	</div>
