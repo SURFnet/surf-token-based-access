ac310d7ef7ca47f3b64844fa3912023a1dffa806
Pieter van der Meulen <pieter.vandermeulen@surf.nl>
2024-12-17 13:09:52 +0100

    Add build script

---
+++ phase-3/spec/build-rfc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+MONODRAW=/Applications/Monodraw.app/Contents/Resources/monodraw
+XML2RFC=xml2rfc
+
+# If Monodraw is installed, use it to generate the RFC diagram
+if [ -x "$MONODRAW" ]; then
+  "$MONODRAW" seq.monopic > seq.txt;
+else
+  echo "Monodraw is not installed. Skipping RFC diagram generation."
+fi
+
+# Generate the RFC
+"$XML2RFC" draft-vandermeulen-oauth-resource-helper-00.xml --text
